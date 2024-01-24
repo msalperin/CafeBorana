8908ee0100b8a4760a8130b78e7f8f24500f9eeb
unknown <msalperin@hotmail.com>
2024-01-24 18:01:29 -0300

    avances

--- cafe-borana/src/components/header/header.js
+++ cafe-borana/src/components/header/header.js
@@ -1,27 +1,40 @@
 import React from "react";
 import './header.css';
-import logo from '../../assets/boranahorizontal.png'
+import logo from '../../assets/boranahorizontal.png';
+import logoBanner from '../../assets/boranaverticalblanco.png';
+import tostadoresdecafe from '../../assets/tostadoresdecafe.png';
 
 const Header = () => {
+
     return (
-       <>
+       
+
+       <header>
+
         <div className="pre-head"><h3>ENVIOS A TODO EL PAIS</h3></div>
         
         <div className="header-conteiner">
 
-               <div className="head-logo"><img src={logo}></img></div>
+               <div className="head-logo"><img src={logo} alt="logo"></img></div>
+               
                <navbar className="navbar-conteiner">
                      <ul>
                         <li>NOSOTROS</li>
                         <li>CONTACTO</li>
-                        <li>TIENDA</li>
+                        <li><button>TIENDA</button></li>
                      </ul>
                </navbar>
                
         </div>
 
-        <div>Banner</div>
-       </>
+        <div className='slider-conteiner'>
+            <img src={logoBanner} alt="borana logo" className='slider-logo'/>
+            <img src={tostadoresdecafe} alt="subtitulo borana" className='slider-slogan' />
+        
+        </div>
+
+
+       </header> 
 
     );
 }
